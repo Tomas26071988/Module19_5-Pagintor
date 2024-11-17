8ed77ccaf42101e4c4ecaa230f5f6bd42f74fa78
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-11-17 21:35:11 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,33 +1,13 @@
-"""
-URL configuration for UrbanDjango project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 from django.views.generic import RedirectView
-from task1 import views
+from .views import sign_up_by_django, sign_up_by_html
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('task1/', include('task1.urls')),
-    path('', include('myblog.urls')),
-    path('', RedirectView.as_view(url='/task1/', permanent=False)),
+    path('', RedirectView.as_view(url='django_sign_up/', permanent=False), name='redirect_sign_up'),
+    path('django_sign_up/', sign_up_by_html, name='sign_up_html'),
+    path('sign_up/', sign_up_by_django, name='sign_up_django'),
+    path('home', views.home, name='home'),
+    path('shop/', views.shop, name='shop'),
+    path('cart/', views.cart, name='cart'),
 ]
-
-
-
-
-
-
