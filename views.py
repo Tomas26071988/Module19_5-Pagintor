1b12bc4a2a8a43f4f7bab1e5beebe1fd629e7e76
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-11-18 00:39:00 +0300

    Add files via upload

--- views.py
+++ views.py
@@ -1,56 +1,16 @@
 from django.shortcuts import render
-from .forms import UserRegister
-from .models import Buyer, Game
-
-def home(request):
-    games = ['Atomic Heart', 'Cyberpunk 2077']
-    return render(request, 'fourth_task/home.html', {'games': games})
-
-def shop(request):
-    items = Game.objects.all()
-    return render(request, 'fourth_task/shop.html', {'items': items})
-
-def cart(request):
-    return render(request, 'fourth_task/cart.html')
-
-# Псевдо-список пользователей
-users = ["user1", "user2", "user3"]
-
-def sign_up_by_django(request):
-    return sign_up_handler(request)
+from django.core.paginator import Paginator
+from .models import Post
 
 
-def sign_up_by_html(request):
-    return sign_up_handler(request)
+def post_list(request):
+    post_list = Post.objects.all()
+    paginator = Paginator('per_page', 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'myblog/post_list.html', {'page_obj': page_obj})
 
 
-def sign_up_handler(request):
-    info = {}
-    form = UserRegister()
-
-    if request.method == "POST":
-        form = UserRegister(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            repeat_password = form.cleaned_data['repeat_password']
-            age = form.cleaned_data['age']
-
-            # Логика проверки
-            if Buyer.objects.filter(username=username).exists():
-                info['error'] = 'Пользователь уже существует'
-            elif age < 18:
-                info['error'] = 'Вы должны быть старше 18'
-            elif password != repeat_password:
-                info['error'] = 'Пароли не совпадают'
-            else:
-                # Если все проверки пройдены
-                Buyer.objects.create(username=username, password=password, age=age)
-                return render(request, 'fifth_task/registration_page.html',
-                              {'info': {'message': f'Приветствуем, {username}!'}})
-
-    info['form'] = form
-    return render(request, 'fifth_task/registration_page.html', {'info': info})
 
 
 # Create your views here.
